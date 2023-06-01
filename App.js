988d6337c95cb3a8097966782991bed36ce779e5
karg315 <110070000+karg315@users.noreply.github.com>
2023-06-01 00:36:52 -0500

    v7.1
    Solución al error del link del deploy, ahora si carga la versión actualizada siempre.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <View style={{ width: "100%", height: "100%" }}>
             <WebView
-                source={{ uri: "https://6477d8b1d4d0e411b2188fbd--zippy-cucurucho-5e804d.netlify.app/" }}
+                source={{ uri: "https://main--zippy-cucurucho-5e804d.netlify.app" }}
                 onLoad={console.log("Si cargó xdxd")}
             ></WebView>
         </View>
